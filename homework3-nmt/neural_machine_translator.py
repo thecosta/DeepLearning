35bf77fe0295ceb7fed95b4dd60758e3555178da
thecosta <bru.costa.bc@gmail.com>
2019-04-21 20:57:12 -0400

    GPU support

--- homework3-nmt/neural_machine_translator.py
+++ homework3-nmt/neural_machine_translator.py
@@ -32,64 +32,65 @@ def nmt(input_tensor_train, input_tensor_val, target_tensor_train, target_tensor
     vocab_inp_size = len(inp_lang.word_index)+1
     vocab_tar_size = len(targ_lang.word_index)+1
 
-    dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
-    dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
-    
-    example_input_batch, example_target_batch = next(iter(dataset))
-    #example_input_batch, example_target_batch = dataset.make_initializable_iterator().get_next()
+    with tf.device("/cpu:0"):
+        dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+        dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+        
+        example_input_batch, example_target_batch = next(iter(dataset))
+        #example_input_batch, example_target_batch = dataset.make_initializable_iterator().get_next()
 
-    # Encoder
-    encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
-    sample_hidden = encoder.initialize_hidden_state()
-    sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
-    print (f'Encoder output shape: (batch size, sequence length, units): {sample_output.shape}')
-    print (f'Encoder Hidden state shape: (batch size, units): {sample_hidden.shape}')
+        # Encoder
+        encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+        sample_hidden = encoder.initialize_hidden_state()
+        sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+        #print (f'Encoder output shape: (batch size, sequence length, units): {sample_output.shape}')
+        #print (f'Encoder Hidden state shape: (batch size, units): {sample_hidden.shape}')
 
-    # Attention layer
-    attention_layer = BahdanauAttention(10)
-    attention_result, attention_weights = attention_layer(sample_hidden, sample_output)
-    print(f'Attention result shape: (batch size, units): {attention_result.shape}')
-    print(f'Attention weights shape: (batch_size, sequence_length, 1): {attention_weights.shape}')    
+        # Attention layer
+        attention_layer = BahdanauAttention(10)
+        attention_result, attention_weights = attention_layer(sample_hidden, sample_output)
+        #print(f'Attention result shape: (batch size, units): {attention_result.shape}')
+        #print(f'Attention weights shape: (batch_size, sequence_length, 1): {attention_weights.shape}')    
 
-    # Decoder
-    decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE)
-    sample_decoder_output, _, _ = decoder(tf.random.uniform((64, 1)),
-                                      sample_hidden, sample_output)
-    print (f'Decoder output shape: (batch_size, vocab size) {sample_decoder_output.shape}')
-    
-    optimizer = tf.keras.optimizers.Adam()
-    #loss_object = tf.keras.metrics.sparse_categorical_crossentropy
-    #loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
-    
-    #checkpoint_dir = './training_checkpoints'
-    #checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    #checkpoint = tf.train.Checkpoint(optimizer=optimizer,
-    #                                 encoder=encoder,
-    #                                 decoder=decoder)
-    
-    EPOCHS = 10
+        # Decoder
+        decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE)
+        sample_decoder_output, _, _ = decoder(tf.random.uniform((64, 1)),
+                                          sample_hidden, sample_output)
+        #print (f'Decoder output shape: (batch_size, vocab size) {sample_decoder_output.shape}')
+        
+        optimizer = tf.keras.optimizers.Adam()
+        #loss_object = tf.keras.metrics.sparse_categorical_crossentropy
+        #loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+        
+        #checkpoint_dir = './training_checkpoints'
+        #checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+        #checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+        #                                 encoder=encoder,
+        #                                 decoder=decoder)
+        
+        EPOCHS = 10
 
-    for epoch in range(EPOCHS):
-        start = time.time()
+        for epoch in range(EPOCHS):
+            start = time.time()
 
-        enc_hidden = encoder.initialize_hidden_state()
-        total_loss = 0
+            enc_hidden = encoder.initialize_hidden_state()
+            total_loss = 0
 
-        for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
-            batch_loss = train_step(inp, targ, enc_hidden, encoder, decoder, targ_lang, optimizer, BATCH_SIZE)
-            total_loss += batch_loss
+            for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+                batch_loss = train_step(inp, targ, enc_hidden, encoder, decoder, targ_lang, optimizer, BATCH_SIZE)
+                total_loss += batch_loss
 
-            if batch % 100 == 0:
-                print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
-                                                         batch,
-                                                         batch_loss.numpy()))
-        # saving (checkpoint) the model every 2 epochs
-        #if (epoch + 1) % 2 == 0:
-        #    checkpoint.save(file_prefix = checkpoint_prefix)
+                if batch % 100 == 0:
+                    print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                             batch,
+                                                             batch_loss.numpy()))
+            # saving (checkpoint) the model every 2 epochs
+            #if (epoch + 1) % 2 == 0:
+            #    checkpoint.save(file_prefix = checkpoint_prefix)
 
-        print('Epoch {} Loss {:.4f}'.format(epoch + 1,
-                                          total_loss / steps_per_epoch))
-        print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+            print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                              total_loss / steps_per_epoch))
+            print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
 
 
 def train_step(inp, targ, enc_hidden, encoder, decoder, targ_lang, optimizer, BATCH_SIZE):
@@ -123,7 +124,12 @@ if __name__ == '__main__':
     parser.add_argument('--data_path', type=str,
                         default='./data/')
     parser.add_argument('--num_examples', type=int, default=30000)
+    parser.add_argument('--id_gpu', type=int)
     args = parser.parse_args()
+
+    if args.id_gpu >= 0:
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.id_gpu
     
     print('Processing and loading data...')
     input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val, inp_lang, target_lang = preprocess(args.data_path, args.num_examples)
